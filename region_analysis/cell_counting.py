05f8c9f0b859b890c1165ddf216f0dc7b02da949
pmantini <m_pranav@yahoo.com>
2024-06-20 09:04:22 -0500

    Initial commit

---
+++ region_analysis/cell_counting.py
@@ -0,0 +1,38 @@
+from dip import *
+
+class CellCounting:
+    def __init__(self):
+        pass
+
+    def blob_coloring(self, image):
+        """Implement the blob coloring algorithm
+        takes a input:
+        image: binary image
+        return: a list/dict of regions"""
+
+        regions = dict()
+
+        return regions
+
+    def compute_statistics(self, region):
+        """Compute cell statistics area and location
+        takes as input
+        region: a list/dict of pixels in a region
+        returns: region statistics"""
+
+        # Please print your region statistics to stdout
+        # <region number>: <location or center>, <area>
+        # print(stats)
+
+        return 0
+
+    def mark_image_regions(self, image, stats):
+        """Creates a new image with computed stats
+        Make a copy of the image on which you can write text. 
+        takes as input
+        image: a list/dict of pixels in a region
+        stats: stats regarding location and area
+        returns: image marked with center and area"""
+
+        return image
+
