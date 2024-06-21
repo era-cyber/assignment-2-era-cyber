8949cb8eb377dbc1fc0f812150a40fc53bacb10b
pmantini <m_pranav@yahoo.com>
2024-06-21 09:07:43 -0500

    Update cell_counting.py

--- region_analysis/cell_counting.py
+++ region_analysis/cell_counting.py
@@ -5,20 +5,20 @@ class CellCounting:
         pass
 
     def blob_coloring(self, image):
-        """Implement the blob coloring algorithm
-        takes a input:
+        """ Implement the blob coloring algorithm
+        takes as input:
         image: binary image
-        return: a list/dict of regions"""
+        returns: a list/dictionary of regions """
 
         regions = dict()
 
         return regions
 
     def compute_statistics(self, region):
-        """Compute cell statistics area and location
+        """ Compute cell statistics area and location
         takes as input
-        region: a list/dict of pixels in a region
-        returns: region statistics"""
+        region: a list/dictionary of pixels in a region
+        returns: region statistics """
 
         # Please print your region statistics to stdout
         # <region number>: <location or center>, <area>
@@ -27,12 +27,12 @@ class CellCounting:
         return 0
 
     def mark_image_regions(self, image, stats):
-        """Creates a new image with computed stats
+        """ Creates a new image with computed stats
         Make a copy of the image on which you can write text. 
         takes as input
-        image: a list/dict of pixels in a region
+        image: a list/dictionary of pixels in a region
         stats: stats regarding location and area
-        returns: image marked with center and area"""
+        returns: image marked with center and area """
 
         return image
 
